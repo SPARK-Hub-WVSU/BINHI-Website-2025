d6939085a868be263e2a7e936a64fd1c5ec85561
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2025-04-11 09:28:52 +0800

    feat: Added title header on contacts page

---
+++ src/app/contacts/page.js
@@ -0,0 +1,15 @@
+
+
+export default function Contacts() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-12 sm:gap-6 w-full max-w-screen py-10 sm:py-5">
+        {/* TITLE */}
+        <section className="mt-[6rem] w-full max-w-[60rem] px-8 sm:px-4">
+            <h1 className="text-center">Ready to launch your next startup?</h1>
+            <p className="mt-6 text-justify">
+                Inquire for incubation by sending us a letter through our contact details. We are also open to collaborate with non-profits, educational institutes, and organizations in promoting the startup ecosystem through meetups, seminars, and conferences.
+            </p>
+        </section>
+    </div>
+  );
+}
