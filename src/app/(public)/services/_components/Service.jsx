e36c65d83601a9315bfe7da16c786b5bdbd1bbaa
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2025-05-06 09:45:40 +0800

    Applied requested changes in services page
    Finished Tasks:
    - Shrunk service titles for mobile devices
    - Merged title and services list as one section
    - Changed services to <li> and <ul>

--- src/app/(public)/services/_components/Service.jsx
+++ src/app/(public)/services/_components/Service.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-export default function Service({icon, name, desc}) {
+export default function Service({ icon, name, desc }) {
     return (
-        <div className="flex flex-col gap-y-2">
+        <li className="flex flex-col gap-y-2">
             <div className="flex flex-row gap-x-4 items-center">
-                <Image src={icon} className="size-[2rem]" alt="icon"/>
-                <h2 className="text-2xl">{name}</h2>
+                <Image src={icon} className="size-[2rem]" alt={`${name} icon`} />
+                <h2 className="text-lg lg:text-xl font-semibold">{name}</h2>
             </div>
-            <p className="text-2xl">{desc}</p>
-        </div>
+            <p className="text-lg lg:text-xl">{desc}</p>
+        </li>
     );
 }
--- src/app/(public)/services/page.js
+++ src/app/(public)/services/page.js
@@ -7,36 +7,38 @@ export default function Services() {
     <div className="flex flex-col items-center justify-center gap-12 sm:gap-6 py-10 sm:py-5">
 
         {/* TITLE */}
-        <section className="max-w-[60rem] px-8 sm:px-4">
-            <h1 className="text-center">Services</h1>
-            <p className="mt-6 text-center">Startup incubatees at BINHI are entitled to the following services:</p>
-        </section>
-
-        <section className="p-[2.5rem] flex flex-col lg:flex-row gap-x-5">
-            <div className="max-w-[33rem] flex flex-col gap-y-5">
-                {
-                    servicesData[0].map((service, index) => (
-                        <Service
-                            key={index}
-                            icon={service.icon}
-                            name={service.name}
-                            desc={service.desc}
-                        />
-                    ))
-                }
+        <section id="services-list">
+            <div className="max-w-[60rem] px-8 sm:px-4">
+                <h1 className="text-center">Services</h1>
+                <p className="mt-6 text-center">Startup incubatees at BINHI are entitled to the following services:</p>
             </div>
 
-            <div className="max-w-[33rem] flex flex-col gap-y-5">
-                {
-                    servicesData[1].map((service, index) => (
-                        <Service
-                            key={index}
-                            icon={service.icon}
-                            name={service.name}
-                            desc={service.desc}
-                        />
-                    ))
-                }
+            <div className="p-[2.5rem] flex flex-col lg:flex-row gap-x-5">
+                <ul className="max-w-[33rem] flex flex-col gap-y-5">
+                    {
+                        servicesData[0].map((service, index) => (
+                            <Service
+                                key={index}
+                                icon={service.icon}
+                                name={service.name}
+                                desc={service.desc}
+                            />
+                        ))
+                    }
+                </ul>
+
+                <ul className="max-w-[33rem] flex flex-col gap-y-5">
+                    {
+                        servicesData[1].map((service, index) => (
+                            <Service
+                                key={index}
+                                icon={service.icon}
+                                name={service.name}
+                                desc={service.desc}
+                            />
+                        ))  
+                    }
+                </ul>
             </div>
         </section>
 
