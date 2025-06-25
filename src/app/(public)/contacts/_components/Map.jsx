80db34f5d0af808a43d5bed9bd335b740b74f836
Manns <126627743+hydraadra112@users.noreply.github.com>
2025-06-25 16:38:34 +0800

    Merge frontend/contacts to main (#7)
    * Draft portfolio page
    
    * feat: Added title header on contacts page
    
    * feat: Added contacts and updates
    
    * refactor: Separated contact details
    
    * feat: Added send message component
    
    * feat: Added interactive map using leaflet
    
    * feat: Created contact us page
    
    * refactor: applied requested changes on contacts page
    
    * refactor: converted graduates & incubates array to object for cleaner mapping
    
    * refactor: defined message box bg color in global.css and deleted contacts.css
    
    * styles: redefined style of contact us box
    
    * chore: changed import style
    
    * applied custom css on contact us page
    
    * Make contacts page responsive to mobile size devices
    
    * styles(contacts): adjust spacing and sizes to follow figma wireframe
    
    ---------
    
    Co-authored-by: Rei Ebenezer Duhina <126635968+Reiebenezer@users.noreply.github.com>

---
+++ src/app/(public)/contacts/_components/Map.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const customIcon = new L.Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+  iconSize: [30, 30],
+  iconAnchor: [15, 30],
+  popupAnchor: [0, -30],
+});
+
+export default function Map() {
+  const markers = [
+    {
+      coords: [10.712438277476279, 122.56034164962979],
+      popUp: "BINHI Facility, WVSU TBI",
+    },
+  ];
+
+  return (
+    <MapContainer center={markers[0].coords} zoom={18} className="rounded-3xl shadow-2xl w-[27rem] h-[23rem] md:h-[40rem]">
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {markers.map((marker, index) => (
+        <Marker key={index} position={marker.coords} icon={customIcon}>
+          <Popup>
+            <p className="font-bold text-primary text-sm">{marker.popUp}</p>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
