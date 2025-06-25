d4dfe482c027be873ded37b3cd5796f075b36ed1
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2025-06-26 00:20:51 +0800

    Merge branch 'main' into frontend/homepage

---
+++ src/app/news/InsetArticle.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Image from "next/image";
+
+import placeholderPhoto from '@/assets/placeholder-photo.png';
+import Link from "next/link";
+
+export default function InsetArticle({ data }) {
+    return (
+        <Link className="group min-h-50 relative isolate overflow-hidden flex flex-col justify-end p-4 gap-2 after:content-[''] after:absolute after:inset-0 after:-z-[1] after:bg-linear-to-t after:from-black after:from-[-20%] after:to-transparent" href={`/article/${data.id}`} >
+            <Image src={data.image || placeholderPhoto} alt={data.headline} className="absolute inset-0 object-center object-cover w-full h-full -z-[2]" />
+            <h2 className="text-white font-semibold text-xl group-hover:text-primary transition-colors">{data.headline}</h2>
+            <p className="text-white text-base">{new Date(data.date).toLocaleDateString('en-us', { dateStyle: 'long' })}</p>
+        </Link>
+    );
+}
