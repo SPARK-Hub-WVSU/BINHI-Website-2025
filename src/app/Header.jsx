04cb764e19de47ca4a8fab3d5730a7dc8249e557
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-06-25 15:52:13 +0800

    ref: wrapped nav w header; wrapped sections w main; size adjustments

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -6,39 +6,40 @@ import Link from "next/link"
 export default function Header() {
     return (
 
-        <nav className="h-20 max-w-280 lg:max-w-450 mx-auto 
+        <header>
+            <nav className="h-20 max-w-280 lg:max-w-450 mx-auto 
         flex flex-col md:flex-row  mt-4 md:mt-0 md:justify-between md:items-center ">
 
-            <div className="flex w-screen sm:w-fit justify-center  mx-auto">
-                <Logo version={1}></Logo>
-            </div>
+                <div className="flex w-screen sm:w-fit justify-center  mx-auto">
+                    <Logo version={1}></Logo>
+                </div>
 
-            <ul className="flex space-x-4 mt-4 md:mt-0
+                <ul className="flex space-x-4 mt-4 md:mt-0
             sm:space-x-10 mx-auto text-base font-medium 
             ">
-                <li>
-                    <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
-                        About BINHI
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
-                        Services
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            About BINHI
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            Services
 
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
-                        News
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
-                        Contact Us
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            News
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            Contact Us
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -24,200 +24,195 @@ import meetingFacilities from '@/assets/startup-starters/meeting-facilities.svg'
 
 export default function Home() {
   return (
-    <>
-      <div className="">
 
-        <Header />
 
-        {/* ------------------- HERO -------------------*/}
-        <section className="mt-10 md:mt-5 h-125 sm:h-150 md:h-175
+    <main>
+      {/* ------------------- HERO -------------------*/}
+      <section className="mt-10 md:mt-5 h-125 sm:h-150 md:h-175
         lg:h-screen xl:min-h-screen 
         bg-no-repeat bg-cover bg-center
         
         "
-          style={{
-            backgroundImage: `url(${heroImage.src})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center',
-          }}
-        >
+        style={{
+          backgroundImage: `url(${heroImage.src})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+        }}
+      >
 
 
-          <div className=" pt-10 md:pt-20 lg:pt-35 pl-5 md:pl-25 lg:pl-42 w-fit ">
-            <h1 className="max-w-153 mb-5 text-secondary font-bold
+        <div className=" pt-10 md:pt-20 lg:pt-35 pl-5 md:pl-25 lg:pl-42 w-fit ">
+          <h1 className="max-w-153 mb-5 text-secondary font-bold
             text-3xl md:text-4xl lg:text-5xl
             md:leading-14 lg:leading-18 ">
-              Nurturing Seeds of<br /> Ideas <span className="text-primary-dark">One Step <br />at a Time.</span>
-            </h1>
+            Nurturing Seeds of<br /> Ideas <span className="text-primary-dark">One Step <br />at a Time.</span>
+          </h1>
 
-            <LinkButton href="/">Incubate your startup with us</LinkButton>
-          </div>
-        </section>
-
-
-        {/* ------------------- MISSION  -------------------*/}
-        <section className="relative ">
-          <div className="w-full h-110 md:h-185 mission absolute -top-2 md:-top-5 z-0"></div>
+          <LinkButton href="/">Incubate your startup with us</LinkButton>
+        </div>
+      </section>
 
 
-          <section className="relative z-10 h-fit mx-10
+      {/* ------------------- MISSION  -------------------*/}
+      <section className="relative ">
+        <div className="w-full h-110 md:h-185 mission absolute -top-2 md:-top-5 z-0"></div>
+
+
+        <section className="relative z-10 h-fit mx-10
           flex flex-col justify-center items-center "
-          >
+        >
 
-            <div className="mt-10 md:mt-30 max-w-200">
-              <h2 className="text-primary-dark font-bold text-2xl md:text-3xl lg:text-4xl text-center">
-                Our Mission
-              </h2>
-              <p className="mt-4 text-center md:text-justify text-xl md:text-2xl">
-                is to
-                <span className="text-primary-dark font-semibold">
-                  &nbsp;empower innovation and economic growth by inspiring and nurturing technology-based startups
-                </span>
-                , fostering collaboration between academia, industry, and government, and advancing the commercialization of local technologies in the region.
-              </p>
-            </div>
-
-
-            <div className="mt-20 md:mt-30 max-w-200 ">
-              <h2 className="text-primary-dark font-bold text-2xl md:text-3xl lg:text-4xl text-center">
-                We Envision
-              </h2>
-              <p className="mt-4 text-center md:text-justify text-xl md:text-2xl" >
-                an ecosystem where
-                <span className="text-primary-dark font-semibold">
-                  &nbsp;technology-based startups flourish, local innovations prosper, and collaboration propels regional economic growth
-                </span>
-                , establishing BINHI as a respected leader in enterprising innovations.
-              </p>
-            </div>
+          <div className="mt-10 md:mt-30 max-w-200">
+            <h2 className="text-primary-dark font-bold text-2xl md:text-3xl lg:text-4xl text-center">
+              Our Mission
+            </h2>
+            <p className="mt-4 text-center md:text-justify text-xl md:text-2xl">
+              is to
+              <span className="text-primary-dark font-semibold">
+                &nbsp;empower innovation and economic growth by inspiring and nurturing technology-based startups
+              </span>
+              , fostering collaboration between academia, industry, and government, and advancing the commercialization of local technologies in the region.
+            </p>
+          </div>
 
 
-            <div className="max-w-200 mt-20 md:mt-30 flex flex-col justify-between items-center">
-              <h2 className="text-primary-dark font-bold text-3xl md:text-4xl text-center">
-                BINHI accepts and nurtures startups in four key sectors:
-              </h2>
-
-              <ul className="mt-8 md:mt-12">
-                <li className="flex items-center text-xl md:text-2xl mb-5 md:mb-7">
-                  <HeartIcon className="text-primary-dark mr-5 md:mr-8 size-10" />
-                  Biomedical and Healthcare
-                </li>
-                <li className="flex items-center text-xl md:text-2xl mb-5 md:mb-7">
-                  <GlobeAsiaAustraliaIcon weight="fill" className="text-primary-dark mr-5 md:mr-8 size-10" />
-                  Agri-Aqua and Green Technologies
-                </li>
-                <li className="flex items-center text-xl md:text-2xl mb-5 md:mb-7">
-                  <ComputerDesktopIcon className="text-primary-dark mr-5 md:mr-8 size-10" />
-                  Emerging Technologies / ICT
-                </li>
-                <li className="flex items-center text-xl md:text-2xl ">
-                  <BeakerIcon className="text-primary-dark mr-5 md:mr-8 size-10" />
-                  Herbal & Natural Products
-                </li>
-              </ul>
-            </div>
-          </section >
-        </section>
+          <div className="mt-20 md:mt-30 max-w-200 ">
+            <h2 className="text-primary-dark font-bold text-2xl md:text-3xl lg:text-4xl text-center">
+              We Envision
+            </h2>
+            <p className="mt-4 text-center md:text-justify text-xl md:text-2xl" >
+              an ecosystem where
+              <span className="text-primary-dark font-semibold">
+                &nbsp;technology-based startups flourish, local innovations prosper, and collaboration propels regional economic growth
+              </span>
+              , establishing BINHI as a respected leader in enterprising innovations.
+            </p>
+          </div>
 
 
-        {/* -------------- STARTUP STARTERS  ---------------*/}
-        <section className="relative bg-blue-300">
-
-          <Image
-            alt="Building Background Image"
-            className="absolute inset-0 w-full h-full bg-no-repeat bg-cover bg-center"
-            src={buildingBg.src}
-            fill
-          />
-
-          {/* overlay with opacity */}
-          <div className="absolute inset-0 bg-white/60" />
-
-          {/* content - will be above the overlay */}
-          <div className="relative z-10 h-353">
-
-            <div className="h-94 absolute inset-0 bg-gradient-to-b from-white via-white to-transparent"
-            />
-
-            <h2 className="relative pt-55 z-10 max-w-187 font-bold text-5xl text-primary mx-auto text-center
-            mb-12">
-              We also provide these starters for your startup:
+          <div className="max-w-200 mt-20 md:mt-30 lg:mt-40 flex flex-col justify-between items-center">
+            <h2 className="text-primary-dark font-bold text-3xl md:text-4xl text-center">
+              BINHI accepts and nurtures startups in four key sectors:
             </h2>
 
-            <section className="mx-auto max-w-287 flex flex-wrap justify-center
+            <ul className="mt-8 md:mt-12">
+              <li className="flex items-center text-xl md:text-2xl mb-5 md:mb-7">
+                <HeartIcon className="text-primary-dark mr-5 md:mr-8 size-10" />
+                Biomedical and Healthcare
+              </li>
+              <li className="flex items-center text-xl md:text-2xl mb-5 md:mb-7">
+                <GlobeAsiaAustraliaIcon weight="fill" className="text-primary-dark mr-5 md:mr-8 size-10" />
+                Agri-Aqua and Green Technologies
+              </li>
+              <li className="flex items-center text-xl md:text-2xl mb-5 md:mb-7">
+                <ComputerDesktopIcon className="text-primary-dark mr-5 md:mr-8 size-10" />
+                Emerging Technologies / ICT
+              </li>
+              <li className="flex items-center text-xl md:text-2xl ">
+                <BeakerIcon className="text-primary-dark mr-5 md:mr-8 size-10" />
+                Herbal & Natural Products
+              </li>
+            </ul>
+          </div>
+        </section >
+      </section>
+
+
+      {/* -------------- STARTUP STARTERS  ---------------*/}
+      <section className="relative bg-blue-300">
+
+        <Image
+          alt="Building Background Image"
+          className="absolute inset-0 w-full h-full bg-no-repeat bg-cover bg-center"
+          src={buildingBg.src}
+          fill
+        />
+
+        {/* overlay with opacity */}
+        <div className="absolute inset-0 bg-white/60" />
+
+        {/* content - will be above the overlay */}
+        <div className="relative z-10 h-353">
+
+          <div className="h-94 absolute inset-0 bg-gradient-to-b from-white via-white to-transparent"
+          />
+
+          <h2 className="relative pt-55 z-10 max-w-187 font-bold text-2xl md:text-3xl lg:text-4xl 
+            text-primary mx-auto text-center mb-12">
+            We also provide these starters for your startup:
+          </h2>
+
+          <section className="mx-auto max-w-288 flex flex-wrap justify-center
             ">
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={inHouse} className="mb-6" alt="In-house and Virtual Incubation Icon" />
-                <span >In-house and Virtual Incubation</span>
-              </div>
+              <Image src={inHouse} className="mb-6" alt="In-house and Virtual Incubation Icon" />
+              <span >In-house and Virtual Incubation</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={businessAssistance} className="mb-6" alt="Business and Professional Assistance Icon" />
-                <span>Business and Professional Assistance</span>
-              </div>
+              <Image src={businessAssistance} className="mb-6" alt="Business and Professional Assistance Icon" />
+              <span>Business and Professional Assistance</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={consultancySupport} className="mb-6" alt="Consultancy and Support Services Icon" />
-                <span>Consultancy and Support Services</span>
-              </div>
+              <Image src={consultancySupport} className="mb-6" alt="Consultancy and Support Services Icon" />
+              <span>Consultancy and Support Services</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={fundingAssistance} className="mb-6" alt="Funding and Grants Proposal Assistance Icon" />
-                <span >Funding and Grants Proposal Assistance</span>
-              </div>
+              <Image src={fundingAssistance} className="mb-6" alt="Funding and Grants Proposal Assistance Icon" />
+              <span >Funding and Grants Proposal Assistance</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={monitoringCoaching} className="mb-6" alt="Monitoring and Coaching Icon" />
-                <span >Monitoring & Coaching</span>
-              </div>
+              <Image src={monitoringCoaching} className="mb-6" alt="Monitoring and Coaching Icon" />
+              <span >Monitoring & Coaching</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={technologyForecasting} className="mb-6" alt="Technology Forecasting and Matching Icon" />
-                <span>Technology Forecasting and Matching</span>
-              </div>
+              <Image src={technologyForecasting} className="mb-6" alt="Technology Forecasting and Matching Icon" />
+              <span>Technology Forecasting and Matching</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={laboratoryFacilities} className="mb-6" alt="Laboratory and Production Facilities Icon" />
-                <span>Laboratory and Production Facilities</span>
-              </div>
+              <Image src={laboratoryFacilities} className="mb-6" alt="Laboratory and Production Facilities Icon" />
+              <span>Laboratory and Production Facilities</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <Image src={meetingFacilities} className="mb-6" alt="Meeting and Conference Facilities Icon" />
-                <span>Meeting and Conference Facilities</span>
-              </div>
+              <Image src={meetingFacilities} className="mb-6" alt="Meeting and Conference Facilities Icon" />
+              <span>Meeting and Conference Facilities</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <ScaleIcon className="text-primary size-31 mb-6" stroke="currentColor" />
-                <span>Branding and IP Protection</span>
-              </div>
+              <ScaleIcon className="text-primary size-31 mb-6" stroke="currentColor" />
+              <span>Branding and IP Protection</span>
+            </div>
 
-              <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
+            <div className="bg-white starters-container text-xl font-medium w-full max-w-62 h-73 m-5 
               rounded-3xl py-8 px-4 text-center flex flex-col items-center">
-                <AcademicCapIcon className="text-primary size-31 mb-6" stroke="currentColor" />
-                <span>Trainings and Workshops</span>
-              </div>
+              <AcademicCapIcon className="text-primary size-31 mb-6" stroke="currentColor" />
+              <span>Trainings and Workshops</span>
+            </div>
 
-            </section>
+          </section>
 
 
 
-          </div>
-          <Image src={bgStartupStarters} alt="" className="w-full" />
-        </section>
+        </div>
+        <Image src={bgStartupStarters} alt="" className="w-full" />
+      </section>
+    </main>
 
-        <Footer />
-      </div >
-
-    </>
   );
 }
