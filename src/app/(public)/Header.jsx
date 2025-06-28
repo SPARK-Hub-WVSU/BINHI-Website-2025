7e9571b86ecc7c528c22295598a091a194e65448
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2025-06-28 19:22:19 +0800

    fix: applied edited changes into (public)

--- src/app/(public)/Header.jsx
+++ src/app/(public)/Header.jsx
@@ -2,41 +2,44 @@ import Image from "next/image"
 import Logo from "@/components/Logo"
 import Link from "next/link"
 
+
 export default function Header() {
     return (
 
-        <nav className="h-[80px] max-w-[1120px] mx-auto
-        flex flex-wrap mt-2 sm:mt-0 sm:justify-between sm:items-center ">
+        <header>
+            <nav className="h-20 max-w-280 lg:max-w-450 mx-auto 
+        flex flex-col md:flex-row  mt-4 md:mt-0 md:justify-between md:items-center ">
 
-            <div className="flex w-screen sm:w-fit justify-center  ">
-                <Logo version={1}></Logo>
-            </div>
+                <div className="flex w-screen sm:w-fit justify-center  mx-auto">
+                    <Logo version={1}></Logo>
+                </div>
 
-            <ul className="flex space-x-4 sm:space-x-10 mx-auto sm:mx-0 
-            text-base font-medium">
-                <li>
-                    <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
-                        About BINHI
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/services"} className="text-secondary-neutral hover:text-gray-400">
-                        Services
+                <ul className="flex space-x-4 mt-4 md:mt-0
+            sm:space-x-10 mx-auto text-base font-medium 
+            ">
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            About BINHI
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            Services
 
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
-                        News
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/contacts"} className="text-secondary-neutral hover:text-gray-400">
-                        Contact Us
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            News
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={"/"} className="text-secondary-neutral hover:text-gray-400">
+                            Contact Us
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
