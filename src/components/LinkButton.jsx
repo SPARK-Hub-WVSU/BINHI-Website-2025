25249302af7d2523bb916e7e064b69806730e7b8
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-07-01 00:15:37 +0800

    feat: added updated LinkButton code; was not added previously

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { ArrowRightIcon } from '@heroicons/react/24/solid';
 
 export default function LinkButton({ href, children }) {
@@ -7,12 +6,12 @@ export default function LinkButton({ href, children }) {
         <Link
             href={href}
             className="
-                w-fit h-fit py-4 px-6 flex-shrink-0 rounded-[50px] bg-primary
-                text-white flex items-center justify-center text-2xl font-bold"
+                w-48 md:w-fit h-fit py-2 md:py-4 px-4 md:px-6 flex-shrink-0 rounded-3xl md:rounded-[3rem] bg-primary
+                text-white flex items-center justify-center text-base md:text-xl font-bold"
         >
             {children}
 
-            <ArrowRightIcon className="ml-[22px] w-[24px] h-[24px] text-white stroke-[2px]" stroke="currentColor" />
+            <ArrowRightIcon className="ml-3 size-6 text-white stroke-2" stroke="currentColor" />
         </Link>
     );
 }
