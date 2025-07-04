98da8e731c99877da38f0d06e3968ad5c5ee2ba7
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2025-07-04 20:01:12 +0800

    update content in portfolio page

--- src/app/(public)/startup_portfolio/_data/incubatees.js
+++ src/app/(public)/startup_portfolio/_data/incubatees.js
@@ -1,29 +1,37 @@
-import cnc from '@/assets/portfolio/incubatees/01_cnc.png';
-import logo2 from '@/assets/portfolio/incubatees/02.png';
-import Jaeyels from '@/assets/portfolio/incubatees/03.png';
-import logo4 from '@/assets/portfolio/incubatees/04.png';
-import foodie from '@/assets/portfolio/incubatees/05.png';
-import dexuva from '@/assets/portfolio/incubatees/06.png';
-import dalubasa from '@/assets/portfolio/incubatees/07.png';
-import magic from '@/assets/portfolio/incubatees/08.png';
-import epaul from '@/assets/portfolio/incubatees/09.png';
-import kaldi from '@/assets/portfolio/incubatees/10.png';
-import proventous from '@/assets/portfolio/incubatees/11.png';
-import nutrilink from '@/assets/portfolio/incubatees/12.png';
-import asterix from '@/assets/portfolio/incubatees/13.png';
+import agriSensei from '@/assets/portfolio/incubatees/01AgriSensei.png';
+import stinglessBees from '@/assets/portfolio/incubatees/02StinglessBeesAgriculturalFarm.png';
+import epaultech from '@/assets/portfolio/incubatees/03ePaulTech.png';
+import dalubasa from '@/assets/portfolio/incubatees/04Dalubasa.png';
+import mumHarvest from '@/assets/portfolio/incubatees/05MumHarvestHome.png';
+import dexuva from '@/assets/portfolio/incubatees/06Dexuva.png';
+import buligAI from '@/assets/portfolio/incubatees/07Bulig.AI.png';
+import kaldi from '@/assets/portfolio/incubatees/08PaulKaldiLocallySourcedCoffeeBeans.png';
+import digihwy from '@/assets/portfolio/incubatees/09DigiHwyInc.png';
+import cnc from '@/assets/portfolio/incubatees/10CebuNutritionClinic.png';
+import jaeyels from '@/assets/portfolio/incubatees/11Jaeyel\'sAgri-ProductsManufacturing.png';
+import magic from '@/assets/portfolio/incubatees/12MagicOilAndRub.png';
+import zenlabs from '@/assets/portfolio/incubatees/13ZenlabsTechnology.png';
+import foodie from '@/assets/portfolio/incubatees/14Foodie.png';
+import proventous from '@/assets/portfolio/incubatees/15Proventous.png';
+import iloiloDirectory from '@/assets/portfolio/incubatees/16IloiloDirectory.png';
+import nutrilink from '@/assets/portfolio/incubatees/17NutrilinkCo.png';
 
 export default {
-  "Cebu Nutrition Clinic": cnc,
-  "logo2": logo2,
-  "Jaeyels": Jaeyels,
-  "logo4": logo4,
-  "Foodie": foodie,
-  "Dexuva": dexuva,
+  "AgriSensei": agriSensei,
+  "Stingless Bees Agricultural Farm": stinglessBees,
+  "ePaulTech Solutions": epaultech,
   "DaluBasa": dalubasa,
-  "Magic Oil & Rub": magic,
-  "ePaulTech Solutions": epaul,
+  "Mum Harvest Home": mumHarvest,
+  "Dexuva": dexuva,
+  "Bulig.AI": buligAI,
   "Paul Kaldi Locally Sourced Coffee Beans": kaldi,
+  "DigiHwy Inc.": digihwy,
+  "Cebu Nutrition Clinic": cnc,
+  "Jaeyel's Agri-Products Manufacturing": jaeyels,
+  "Magic Oil & Rub": magic,
+  "Zenlabs Technology": zenlabs,
+  "Foodie": foodie,
   "Proventous": proventous,
-  "Nutrilink Co.": nutrilink,
-  "Asterix": asterix
+  "Iloilo Directory": iloiloDirectory,
+  "Nutrilink Co.": nutrilink
 };
--- src/app/(public)/startup_portfolio/page.js
+++ src/app/(public)/startup_portfolio/page.js
@@ -9,27 +9,28 @@ import Image from 'next/image';
 
 export default function StartupPortfolio() {
   return (
-    <div className="flex flex-col items-center justify-center gap-12 sm:gap-6 w-full max-w-screen py-10 sm:py-5">
+    <div className="flex flex-col items-center justify-center gap-12 sm:gap-6 w-full max-w-screen md:py-10">
         {/* TITLE */}
-        <section className="mt-[6rem] w-full max-w-[60rem] px-8 sm:px-4">
-            <h1 className="text-center">Startup Portfolio</h1>
-            <p className="mt-6 text-justify">
+        <section className="mt-[6rem] w-full max-w-[60rem] md:px-8 px-4">
+            <h1 className="text-center text-2xl md:text-4xl">Startup Portfolio</h1>
+            <p className="mt-6 text-justify text-lg md:text-2xl">
                 Since its 2015 inauguration, BINHI has fostered the development of more than 20 startups within Iloilo. Presented herein are each one of them as current incubatees and graduates.
             </p>
         </section>
 
         {/* CURRENT INCUBATEES */}
         {/* FIXING IT INTO A GRID IN THE FUTURE */}
-        <section className="flex flex-col gap-12 mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
-            <h2 className="text-center">Current Incubatees</h2>
+        <section className="flex flex-col gap-12 mt-[2rem] md:mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
+            <h2 className="text-center text-2xl md:text-4xl">Current Incubatees</h2>
             <div className="flex flex-wrap items-center justify-center gap-y-10 gap-x-20">
                 {Object.entries(incubatees).map(([name, logo], index) => (
-                <Image
+                <div className="w-[6rem] h-[6rem] md:w-[10rem] md:h-[10rem] flex justify-center items-center">
+                    <Image
                     key={index}
                     src={logo}
                     alt={name}
-                    className="max-h-48 max-w-[15rem]"
-                />
+                    className="h-auto w-auto"
+                /></div>
                 ))}
             </div>
         </section>
@@ -37,24 +38,26 @@ export default function StartupPortfolio() {
 
         {/* GRADUATED STARTUPS */}
         <section className="flex flex-col gap-12 mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
-            <h2 className="text-center">Graduated Startups</h2>
-            <div className="px-5 sm:px-2 flex flex-wrap items-center justify-center gap-4">
-                {
-                Object.entries(graduates).map(([name, logo], index) => (
-                    <div key={index} className="max-h-48 max-w-[15rem] flex items-center justify-center gap-4">
-                    <Image src={logo} alt={name} className="h-full w-auto" />
-                    </div>
-                ))
-                }
+            <h2 className="text-center text-2xl md:text-4xl">Graduated Startups</h2>
+            <div className="px-5 sm:px-2 flex flex-wrap items-center justify-center gap-y-10 gap-x-20">
+                {Object.entries(graduates).map(([name, logo], index) => (
+                <div className="w-[6rem] h-[6rem] md:w-[10rem] md:h-[10rem] flex justify-center items-center">
+                    <Image
+                    key={index}
+                    src={logo}
+                    alt={name}
+                    className="h-auto w-auto"
+                /></div>
+                ))}
             </div>
         </section>
 
 
         {/* SPARKHUB */}
-        <section className="flex flex-col items-center gap-y-[5rem] mt-[5rem] w-full max-w-[60rem] px-8 sm:px-4">
-            <h1 className="text-center">Student Start-up Organization</h1>
+        <section className="flex flex-col items-center gap-y-[2rem] md:gap-y-[5rem] md:mt-[5rem] w-full max-w-[60rem] px-8 sm:px-4">
+            <h1 className="text-center text-2xl md:text-4xl">Student Startup Community</h1>
             <Image alt="SPARK Hub Logo" src={spark} />
-            <p className="mt-6 text-justify">
+            <p className="md:mt-6 text-justify text-lg md:text-2xl">
                 SPARK Hub is an aspiring student-led technopreneurship center at WVSU,
                 where innovators, entrepreneurs, and creatives come together to Create,
                 Collaborate, and Empower-turning ideas into impactful solutions!
@@ -63,13 +66,13 @@ export default function StartupPortfolio() {
             <div className="w-full flex flex-col md:flex-row md:justify-center md:items-center gap-6 md:gap-[8rem]">
                 <button type="button" className="bg-primary rounded-full p-2 gap-x-2 flex flex-row items-center self-start md:self-auto">
                     <Image alt="FB Logo" src={fb} />
-                    <p className="text-base text-white">
+                    <p className="text-base text-white md:text-2xl">
                         spark.hub@wvsu.edu.ph
                     </p>
                 </button>
                 <button type="button" className="bg-primary rounded-full p-2 gap-x-2 flex flex-row items-center self-start md:self-auto">
                     <EnvelopeIcon className="text-background w-[40px] h-[40px]" />
-                    <p className="text-base text-white">
+                    <p className="text-base text-white md:text-2xl">
                         WVSU - SPARK Hub
                     </p>
                 </button>
