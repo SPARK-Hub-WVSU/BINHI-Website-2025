2be6910d780cad37ce443620553c456a92a8bdf6
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2025-07-05 13:08:27 +0800

    refactor(portfolio): make logos bigger in mobile devices

--- src/app/(public)/startup_portfolio/page.js
+++ src/app/(public)/startup_portfolio/page.js
@@ -9,9 +9,9 @@ import Image from 'next/image';
 
 export default function StartupPortfolio() {
   return (
-    <div className="flex flex-col items-center justify-center gap-12 sm:gap-6 w-full max-w-screen md:py-10">
+    <div className="flex flex-col items-center justify-center md:gap-12 gap-6 w-full max-w-screen md:py-10">
         {/* TITLE */}
-        <section className="mt-[6rem] w-full max-w-[60rem] md:px-8 px-4">
+        <section className="mt-[3rem] md:mt-[6rem] w-full max-w-[60rem] md:px-8 px-4">
             <h1 className="text-center text-2xl md:text-4xl">Startup Portfolio</h1>
             <p className="mt-6 text-justify text-lg md:text-2xl">
                 Since its 2015 inauguration, BINHI has fostered the development of more than 20 startups within Iloilo. Presented herein are each one of them as current incubatees and graduates.
@@ -20,42 +20,48 @@ export default function StartupPortfolio() {
 
         {/* CURRENT INCUBATEES */}
         {/* FIXING IT INTO A GRID IN THE FUTURE */}
-        <section className="flex flex-col gap-12 mt-[2rem] md:mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
+        <section className="flex flex-col gap-6 md:gap-12 md:mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
             <h2 className="text-center text-2xl md:text-4xl">Current Incubatees</h2>
-            <div className="flex flex-wrap items-center justify-center gap-y-10 gap-x-20">
-                {Object.entries(incubatees).map(([name, logo], index) => (
-                <div className="w-[6rem] h-[6rem] md:w-[10rem] md:h-[10rem] flex justify-center items-center">
+            <div className="flex flex-wrap justify-center gap-y-3 md:gap-y-6 md:gap-4 md:px-4 md:py-6">
+            {Object.entries(incubatees).map(([name, logo], index) => (
+                <div
+                key={index}
+                className="flex justify-center items-center w-[45%] aspect-[4/3] md:w-[20%] max-w-[13rem]"
+                >
                     <Image
-                    key={index}
-                    src={logo}
-                    alt={name}
-                    className="h-auto w-auto"
-                /></div>
-                ))}
+                        src={logo}
+                        alt={name}
+                        className="max-h-full max-w-full object-contain"
+                    />
+                </div>
+            ))}
             </div>
         </section>
 
 
         {/* GRADUATED STARTUPS */}
-        <section className="flex flex-col gap-12 mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
-            <h2 className="text-center text-2xl md:text-4xl">Graduated Startups</h2>
-            <div className="px-5 sm:px-2 flex flex-wrap items-center justify-center gap-y-10 gap-x-20">
-                {Object.entries(graduates).map(([name, logo], index) => (
-                <div className="w-[6rem] h-[6rem] md:w-[10rem] md:h-[10rem] flex justify-center items-center">
+        <section className="flex flex-col gap-6 md:gap-12 md:mt-[5rem] sm:pt-6 px-5 sm:p-2 w-full max-w-[75rem]">
+            <h2 className="text-center text-2xl md:text-4xl">Graduated Incubatees</h2>
+            <div className="flex flex-wrap justify-center gap-y-3 md:gap-y-6 gap-x-3 md:gap-x-2 md:gap-4 md:px-4 md:py-6">
+            {Object.entries(graduates).map(([name, logo], index) => (
+                <div
+                key={index}
+                className="flex justify-center items-center w-[45%] aspect-[4/3] md:w-[20%] max-w-[13rem]"
+                >
                     <Image
-                    key={index}
-                    src={logo}
-                    alt={name}
-                    className="h-auto w-auto"
-                /></div>
-                ))}
+                        src={logo}
+                        alt={name}
+                        className="max-h-full max-w-full object-contain"
+                    />
+                </div>
+            ))}
             </div>
         </section>
 
 
         {/* SPARKHUB */}
         <section className="flex flex-col items-center gap-y-[2rem] md:gap-y-[5rem] md:mt-[5rem] w-full max-w-[60rem] px-8 sm:px-4">
-            <h1 className="text-center text-2xl md:text-4xl">Student Startup Community</h1>
+            <h1 className="text-center text-2xl md:text-4xl">SPARK Hub Startup Community</h1>
             <Image alt="SPARK Hub Logo" src={spark} />
             <p className="md:mt-6 text-justify text-lg md:text-2xl">
                 SPARK Hub is an aspiring student-led technopreneurship center at WVSU,
