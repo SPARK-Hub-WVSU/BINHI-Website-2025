3c7a57e102cce880e86b31cd0aa1238a191acec0
KirkGamo <gamo.kirkhenrich2005@gmail.com>
2025-07-08 16:51:18 +0800

    refactor: change LabelSection component's span tag to h2 tag for hierarchy

--- src/app/news/LabelSection.jsx
+++ src/app/news/LabelSection.jsx
@@ -11,9 +11,10 @@ import PropTypes from "prop-types";
  */
 export default function LabelSection({ title = "Top Stories" }) {
   return (
-    <div className="inline-block w-fit mb-2 sm:mb-5 px-2 sm:px-4 py-1 sm:py-2 bg-green-600 rounded-tr-[10px] sm:rounded-tr-[20px]">
-      <span className="text-base sm:text-lg md:text-xl lg:text-2xl font-medium text-white">{title}</span>
-    </div>
+      <h2 className="w-fit text-base sm:text-lg md:text-xl lg:text-2xl font-medium text-white 
+        mb-2 sm:mb-5 px-2 sm:px-4 py-1 sm:py-2 bg-green-600 rounded-tr-[10px] sm:rounded-tr-[20px]">
+        {title}
+      </h2>
   );
 }
 
