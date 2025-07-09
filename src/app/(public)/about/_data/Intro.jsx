c3a7b16672f76378f28d2cd23c594cfcbaaae08b
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2025-07-09 13:12:30 +0800

    fix: transformed all .mdx files into .jsx

---
+++ src/app/(public)/about/_data/Intro.jsx
@@ -0,0 +1,23 @@
+export default function Intro() {
+  return (
+    <>
+      <h1>
+        Business Incubator Nurturing <br /> Homegrown Innovations
+      </h1>
+      <p>
+        As{' '}
+        <strong>
+          West Visayas State University’s Technology Business Incubator (TBI)
+        </strong>
+        , BINHI is committed to nurturing startups from seed to early-stage in
+        biomedical and healthcare, agri-aqua and green technologies,
+        natural/herbal products, and emerging technologies/ICT.
+      </p>
+      <p>
+        BINHI aims to contribute to a future where health, sustainability, and
+        technology work together promoting well-being for individuals while
+        protecting and enhancing the environment.
+      </p>
+    </>
+  );
+}
