5e660439fe57b67aa3b72674385f52a843a07e7b
Allyn Ralf Ledesma <110903853+nylla8444@users.noreply.github.com>
2025-07-16 07:45:59 +0800

    style: made LinkButton bigger

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -26,14 +26,17 @@ import { ArrowRightIcon } from '@heroicons/react/24/solid';
 
   */
 
-  
+
 export default function LinkButton({
     href,
     children,
     fullWidthOnMobile = false,
     openInNewTab = true
 }) {
-    const baseClasses = "h-fit py-3 md:py-4 px-6 md:px-6 flex-shrink-0 rounded-3xl md:rounded-[3rem] bg-primary text-white flex items-center justify-center text-base md:text-xl font-bold transition-all duration-200 hover:bg-primary-dark hover:scale-105 hover:shadow-lg group";
+    const baseClasses = `h-fit py-3 px-6 md:py-5 md:px-7 flex-shrink-0 
+    rounded-3xl md:rounded-[3rem] bg-primary text-white
+    flex items-center justify-center text-base md:text-2xl font-bold
+    transition-all duration-200 hover:bg-primary-dark hover:scale-105 hover:shadow-lg group`;
 
     const widthClasses = fullWidthOnMobile
         ? "w-full max-w-85 md:max-w-full md:w-fit"
