bb49f4335ccec93a2323452e7633c627cca6508c
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-07-17 15:45:06 +0800

    feat: add images and update data for timeline

---
+++ src/app/(public)/about/_data/timeline.js
@@ -0,0 +1,64 @@
+import GtbiGroundBreaking from '@/assets/timeline/gtbi-groundbreaking.png'
+import Gtbi2016 from '@/assets/timeline/gtbi-2016.webp'
+import Gtbi2017 from '@/assets/timeline/gtbi-2017.webp'
+import Gtbi2018 from '@/assets/timeline/gtbi-2018.webp'
+import Gtbi2019 from '@/assets/timeline/gtbi-2019.webp'
+import Gtbi2020 from '@/assets/timeline/gtbi-2020.webp'
+import Binhi2022 from '@/assets/timeline/binhi-2022.webp'
+import Binhi2023 from '@/assets/timeline/binhi-2023.webp'
+import Binhi2024 from '@/assets/timeline/binhi-2024.jpg'
+import Binhi2025 from '@/assets/timeline/binhi-2025.png'
+
+
+export default [
+    {
+        "image": GtbiGroundBreaking,
+        "year": "2015",
+        "content": "GTBI was inaugurated, signaling the start of startup creation and workshops."
+    },
+    {
+        "image": Gtbi2016,
+        "year": "2016",
+        "content": "GTBI hosted workshops focusing on innovation and intellectual property."
+    },
+    {
+        "image": Gtbi2017,
+        "year": "2017",
+        "content": "Launching of the website 1.0 and Startup Incubatees took the National and International Stage for Pitching."
+    },
+    {
+        "image": Gtbi2018,
+        "year": "2018",
+        "content": "GTBI faculty participated in mentorship programs and workshops."
+    },
+    {
+        "image": Gtbi2019,
+        "year": "2019",
+        "content": "The TBI 4.0 portal was launched and international academic linkages were established."
+    },
+    {
+        "image": Gtbi2020,
+        "year": "2020",
+        "content": "Adaptation to online learning occurred amidst the COVID-19 pandemic; another incubator was introduced."
+    },
+    {
+        "image": Binhi2022,
+        "year": "2022",
+        "content": "GTBI was rebranded as BINHI; participation in the National Startup Mentor’s Bootcamp."
+    },
+    {
+        "image": Binhi2023,
+        "year": "2023",
+        "content": "Evaluation of BINHI’s Technology Business Incubation Facility and Project Management took place."
+    },
+    {
+        "image": Binhi2024,
+        "year": "2024",
+        "content": "Launching of the website 1.0 and Startup Incubatees took the National and International Stage for Pitching."
+    },
+    {
+        "image": Binhi2025,
+        "year": "2025",
+        "content": "BINHI welcomed 14 startups, offering them free co-working space and virtual incubation support to accelerate their growth. Launched the Seed2Sprout Startup Bootcamp—a four-month program for founders."
+    }
+]
