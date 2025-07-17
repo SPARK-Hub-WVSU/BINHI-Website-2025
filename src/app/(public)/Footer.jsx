58ccf8514710cb034a78a125cb5d68774f241bdb
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-07-17 10:44:34 +0800

    fix: missing href values for the header and footer navigation

--- src/app/(public)/Footer.jsx
+++ src/app/(public)/Footer.jsx
@@ -25,11 +25,11 @@ export default function Footer() {
 
                     {/* Navigation Links */}
                     <div className="flex w-full text-left flex-col space-y-6">
-                        <Link href="/" className="text-base hover:text-primary-light transition-colors">About BINHI TBI</Link>
-                        <Link href="/" className="text-base hover:text-primary-light transition-colors">Services of BINHI TBI</Link>
-                        <Link href="/" className="text-base hover:text-primary-light transition-colors">Contact BINHI TBI</Link>
-                        <Link href="/" className="text-base hover:text-primary-light transition-colors">Startup Portfolio</Link>
-                        <Link href="/" className="text-base hover:text-primary-light transition-colors">News and Events</Link>
+                        <Link href="/about" className="text-base hover:text-primary-light transition-colors">About BINHI TBI</Link>
+                        <Link href="/services" className="text-base hover:text-primary-light transition-colors">Services of BINHI TBI</Link>
+                        <Link href="/contacts" className="text-base hover:text-primary-light transition-colors">Contact BINHI TBI</Link>
+                        <Link href="/startup_portfolio" className="text-base hover:text-primary-light transition-colors">Startup Portfolio</Link>
+                        <Link href="/news" className="text-base hover:text-primary-light transition-colors">News and Events</Link>
                     </div>
 
                     {/* Logo */}
@@ -39,12 +39,12 @@ export default function Footer() {
 
                     {/* Contact Info */}
                     <div className="flex flex-col space-y-5">
-                        <Link href="" className="flex items-center justify-center text-base">
+                        <Link href="mailto:binhi@wvsu.edu.ph" className="flex items-center justify-center text-base">
                             <EnvelopeIcon className="text-primary-light mr-2 size-6" />
                             binhi@wvsu.edu.ph
                         </Link>
 
-                        <Link href="" className="flex items-center justify-center text-base">
+                        <Link href="https://www.facebook.com/wvsubinhi" className="flex items-center justify-center text-base">
                             <Image src={FacebookIcon} className="mr-2 size-6" alt="Facebook Icon" />
                             Follow us on Facebook
                         </Link>
@@ -69,12 +69,12 @@ export default function Footer() {
                         <div className="flex flex-col lg:justify-between lg:h-full mb-8 lg:mb-0">
 
                             <div className="flex flex-col gap-4 mb-6 lg:mb-0">
-                                <Link href="" className="flex items-center text-sm sm:text-base">
+                                <Link href="mailto:binhi@wvsu.edu.ph" className="flex items-center text-sm sm:text-base">
                                     <EnvelopeIcon className="text-primary-light mr-2 size-6 sm:size-8 lg:size-10" />
                                     binhi@wvsu.edu.ph
                                 </Link>
 
-                                <Link href="" className="flex items-center text-sm sm:text-base">
+                                <Link href="https://www.facebook.com/wvsubinhi" className="flex items-center text-sm sm:text-base">
                                     <Image src={FacebookIcon} className="mr-2 size-6 sm:size-8 lg:size-10" alt="Facebook Icon" />
                                     Follow us on Facebook
                                 </Link>
@@ -88,11 +88,11 @@ export default function Footer() {
 
                         {/* Navigation Links */}
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-flow-col lg:grid-rows-3 gap-y-3 sm:gap-y-4 lg:gap-y-6 gap-x-6 sm:gap-x-8 lg:gap-x-12 lg:self-start mb-6 lg:mb-0">
-                            <Link href="/" className="text-sm sm:text-base hover:text-primary-light transition-colors">About BINHI TBI</Link>
-                            <Link href="/" className="text-sm sm:text-base hover:text-primary-light transition-colors">Services of BINHI TBI</Link>
-                            <Link href="/" className="text-sm sm:text-base hover:text-primary-light transition-colors">Contact BINHI TBI</Link>
-                            <Link href="/" className="text-sm sm:text-base hover:text-primary-light transition-colors">Startup Portfolio</Link>
-                            <Link href="/" className="text-sm sm:text-base hover:text-primary-light transition-colors">News and Events</Link>
+                            <Link href="/about" className="text-sm sm:text-base hover:text-primary-light transition-colors">About BINHI TBI</Link>
+                            <Link href="/services" className="text-sm sm:text-base hover:text-primary-light transition-colors">Services of BINHI TBI</Link>
+                            <Link href="/contacts" className="text-sm sm:text-base hover:text-primary-light transition-colors">Contact BINHI TBI</Link>
+                            <Link href="/startup_portfolio" className="text-sm sm:text-base hover:text-primary-light transition-colors">Startup Portfolio</Link>
+                            <Link href="/news" className="text-sm sm:text-base hover:text-primary-light transition-colors">News and Events</Link>
                         </div>
 
                     </section>
