50386a124b320f5f4d548480f72009253c24d870
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-07-20 10:02:21 +0800

    refactor: center align text for tbi officials on about page

--- src/app/(public)/about/_components/OfficerCard.jsx
+++ src/app/(public)/about/_components/OfficerCard.jsx
@@ -7,8 +7,8 @@ export default function OfficerCard({ image, name, title }) {
     return (
         <div className="flex flex-col items-center">
             <Image src={image} alt={name} className="h-[250px] aspect-[3/4] object-contain object-top rounded-3xl object-cover mb-3" />
-            <h3 className="font-bold">{name}</h3>
-            <p className="text-base">{title}</p>
+            <h3 className="font-bold text-center">{name}</h3>
+            <p className="text-base text-center">{title}</p>
         </div>
     );
 }
